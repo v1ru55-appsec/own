b3f54ce24504146be4f8478ed49f2d9f7788f79c
V1ru55Z <32372212+V1ru55Z@users.noreply.github.com>
2017-09-28 22:41:49 +0300

    Create aes_messenger.py

---
+++ aes/aes_messenger.py
@@ -0,0 +1,25 @@
+from aes_lib import *
+
+
+#     3qEbD5~$zkp9Aye?#62grwaNuGnI|t*XBjMZm107{KdSq$BtRU%hy#2F
+#     .tar.gz.gpg
+while True:
+    print("Введите 1 для ЗАШИФРОВКИ текста, Введите 2 для РАСШИФРОВКИ текста")
+    if input() == '1':
+        print("Введите текст который нужно зашифровать, потом нажмите ENTER, потом введите кодовое слово из 16 символов")
+        text = input()
+        key = input()
+        key = replay(key)
+        A = AesCode(key)
+        hash_text = A.encrypt(text)
+    
+        print('Это зашифрованный текст :' + "\n" + hash_text)
+    else :
+        print("Введите текст который нужно расшифровать, потом нажмите ENTER, потом введите кодовое слово из 16 символов")
+        cipher = input()
+        key = input()
+        key = replay(key)
+        A = AesCode(key)
+        hash_text = A.decrypt(cipher)
+    
+        print("Это расшифрованный текст" + "\n" + hash_text)
